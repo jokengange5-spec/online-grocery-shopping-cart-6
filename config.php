e88d3532f1f5806310a7bba70aa7a5e80564e8fc
jokengange5-spec <jokengange5@gmail.com>
2026-04-28 08:24:59 -0700

    Update database connection settings in config.php

--- config.php
+++ config.php
@@ -1,18 +1,23 @@
 <?php
 
-$host = "localhost";
-$db_name = "online_shop";
-$username = "postgres"; // Default Postgres username
-$password = "qwerty";    // Use the password you set during Postgres installation
+$host = "dpg-xxxxxx-a.oregon-postgres.render.com";
+$db_name = "onlineshop_am8u";
+$username = "onlineshop_am8u_user";
+$password = "OquhdogSYiICZFfiWPg0j0cbvume6VIY";
+$port = "5432";
 
 try {
-    // Change 'mysql' to 'pgsql'
-    $conn = new PDO("pgsql:host=$host;dbname=$db_name", $username, $password);
-    
-    // Set error mode to exception so you can see errors clearly
+
+    $conn = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$db_name",
+        $username,
+        $password
+    );
+
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    die("Connection failed: " . $e->getMessage());
 }
 
 ?>
