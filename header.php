16fe7edbe39f52a1edf5df49c33c53d0c9d4cfd5
jokengange5-spec <jokengange5@gmail.com>
2026-04-29 21:28:11 -0700

    Refactor header layout and remove message display
    Removed old message display logic and updated header structure.

--- header.php
+++ header.php
@@ -1,85 +1,15 @@
-<?php
-
-if(isset($message)){
-   foreach($message as $msg){
-      echo '
-      <div class="message">
-         <span>'.$msg.'</span>
-         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
-      </div>
-      ';
-   }
-}
-
-?>
-
 <header class="header">
-
    <div class="flex">
-
-      <!-- LOGO -->
-      <a href="home.php" class="logo">
-   🛒 <span class="brand">Joken's</span> Grocery <span>Shop</span>
-</a>
-
-      <!-- NAVBAR -->
+      <a href="home.php" class="logo">Grocery<span>Store</span></a>
       <nav class="navbar">
          <a href="home.php">home</a>
-          <a href="shop.php">shop</a>
-           <a href="orders.php">order</a>
+         <a href="shop.php">shop</a>
+         <a href="orders.php">orders</a>
          <a href="about.php">about</a>
          <a href="contact.php">contact</a>
       </nav>
-
-      <!-- ICONS -->
       <div class="icons">
-
-         
-
-         <!-- PROFILE ICON (CLICKABLE) -->
-         <a href="update_profile.php" class="fas fa-user"></a>
-
-         <a href="search_page.php" class="fas fa-search"></a>
-
-         <?php
-            $count_cart_items = $conn->prepare("SELECT * FROM cart WHERE user_id = ?");
-            $count_cart_items->execute([$user_id]);
-
-            $count_wishlist_items = $conn->prepare("SELECT * FROM wishlist WHERE user_id = ?");
-            $count_wishlist_items->execute([$user_id]);
-         ?>
-
-         <!-- WISHLIST -->
-         <a href="wishlist.php">
-            <i class="fas fa-heart"></i>
-            <span>(<?= $count_wishlist_items->rowCount(); ?>)</span>
-         </a>
-
-         <!-- CART -->
-         <a href="cart.php">
-            <i class="fas fa-shopping-cart"></i>
-            <span>(<?= $count_cart_items->rowCount(); ?>)</span>
-         </a>
-
+         <!-- search, wishlist, cart, user icons -->
       </div>
-
    </div>
-
 </header>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
