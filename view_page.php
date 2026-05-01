92efb7949eb106399ec2ac15d67e87953e1ee56e
jokengange5-spec <jokengange5@gmail.com>
2026-05-01 10:44:26 -0700

    Update view_page.php

--- view_page.php
+++ view_page.php
@@ -107,7 +107,7 @@ if(isset($_POST['add_to_cart'])){
    ?>
    <form action="" class="box" method="POST">
       <div class="price">₱<span><?= $fetch_products['price']; ?></span>/-</div>
-      <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+      <img src="image products/<?= $fetch_products['image']; ?>" alt="">
       <div class="name"><?= $fetch_products['name']; ?></div>
       <div class="details"><?= $fetch_products['details']; ?></div>
       <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
@@ -147,7 +147,7 @@ if(isset($_POST['add_to_cart'])){
                <form action="" method="POST" class="box">
                   <a href="view_page.php?pid=<?= $fetch_rec['id']; ?>" class="fas fa-eye"></a>
                   <div class="price">₱<span><?= $fetch_rec['price']; ?></span>/-</div>
-                  <img src="uploaded_img/<?= $fetch_rec['image']; ?>" alt="">
+                  <img src="image products/<?= $fetch_rec['image']; ?>" alt="">
                   <div class="name"><?= $fetch_rec['name']; ?></div>
                   <input type="hidden" name="pid" value="<?= $fetch_rec['id']; ?>">
                   <input type="hidden" name="p_name" value="<?= $fetch_rec['name']; ?>">
