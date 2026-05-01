77b2b77e28c5e7eec94adb2905201425ca90b475
jokengange5-spec <jokengange5@gmail.com>
2026-05-01 09:51:41 -0700

    Update search_page.php

--- search_page.php
+++ search_page.php
@@ -8,71 +8,59 @@ $user_id = $_SESSION['user_id'];
 
 if(!isset($user_id)){
    header('location:login.php');
+   exit();
 };
 
-
+// --- LOGIC PARA SA WISHLIST ---
 if(isset($_POST['add_to_wishlist'])){
+   $pid = filter_var($_POST['pid'], FILTER_SANITIZE_STRING);
+   $p_name = filter_var($_POST['p_name'], FILTER_SANITIZE_STRING);
+   $p_price = filter_var($_POST['p_price'], FILTER_SANITIZE_STRING);
+   $p_image = filter_var($_POST['p_image'], FILTER_SANITIZE_STRING);
 
-   $pid = $_POST['pid'];
-   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
-   $p_name = $_POST['p_name'];
-   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
-   $p_price = $_POST['p_price'];
-   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
-   $p_image = $_POST['p_image'];
-   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
+   $check_wishlist = $conn->prepare("SELECT * FROM wishlist WHERE name = ? AND user_id = ?");
+   $check_wishlist->execute([$p_name, $user_id]);
 
-   $check_wishlist_numbers = $conn->prepare("SELECT * FROM wishlist WHERE name = ? AND user_id = ?");
-   $check_wishlist_numbers->execute([$p_name, $user_id]);
+   $check_cart = $conn->prepare("SELECT * FROM cart WHERE name = ? AND user_id = ?");
+   $check_cart->execute([$p_name, $user_id]);
 
-   $check_cart_numbers = $conn->prepare("SELECT * FROM cart WHERE name = ? AND user_id = ?");
-   $check_cart_numbers->execute([$p_name, $user_id]);
-
-   if($check_wishlist_numbers->rowCount() > 0){
-      $message[] = 'already added to wishlist!';
-   }elseif($check_cart_numbers->rowCount() > 0){
-      $message[] = 'already added to cart!';
+   if($check_wishlist->rowCount() > 0){
+      $message[] = 'Already added to wishlist!';
+   }elseif($check_cart->rowCount() > 0){
+      $message[] = 'Already added to cart!';
    }else{
       $insert_wishlist = $conn->prepare("INSERT INTO wishlist (user_id, pid, name, price, image) VALUES(?,?,?,?,?)");
       $insert_wishlist->execute([$user_id, $pid, $p_name, $p_price, $p_image]);
-      $message[] = 'added to wishlist!';
+      $message[] = 'Added to wishlist!';
    }
-
 }
 
+// --- LOGIC PARA SA CART ---
 if(isset($_POST['add_to_cart'])){
+   $pid = filter_var($_POST['pid'], FILTER_SANITIZE_STRING);
+   $p_name = filter_var($_POST['p_name'], FILTER_SANITIZE_STRING);
+   $p_price = filter_var($_POST['p_price'], FILTER_SANITIZE_STRING);
+   $p_image = filter_var($_POST['p_image'], FILTER_SANITIZE_STRING);
+   $p_qty = filter_var($_POST['p_qty'], FILTER_SANITIZE_STRING);
 
-   $pid = $_POST['pid'];
-   $pid = filter_var($pid, FILTER_SANITIZE_STRING);
-   $p_name = $_POST['p_name'];
-   $p_name = filter_var($p_name, FILTER_SANITIZE_STRING);
-   $p_price = $_POST['p_price'];
-   $p_price = filter_var($p_price, FILTER_SANITIZE_STRING);
-   $p_image = $_POST['p_image'];
-   $p_image = filter_var($p_image, FILTER_SANITIZE_STRING);
-   $p_qty = $_POST['p_qty'];
-   $p_qty = filter_var($p_qty, FILTER_SANITIZE_STRING);
+   $check_cart = $conn->prepare("SELECT * FROM cart WHERE name = ? AND user_id = ?");
+   $check_cart->execute([$p_name, $user_id]);
 
-   $check_cart_numbers = $conn->prepare("SELECT * FROM cart WHERE name = ? AND user_id = ?");
-   $check_cart_numbers->execute([$p_name, $user_id]);
-
-   if($check_cart_numbers->rowCount() > 0){
-      $message[] = 'already added to cart!';
+   if($check_cart->rowCount() > 0){
+      $message[] = 'Already added to cart!';
    }else{
+      $check_wishlist = $conn->prepare("SELECT * FROM wishlist WHERE name = ? AND user_id = ?");
+      $check_wishlist->execute([$p_name, $user_id]);
 
-      $check_wishlist_numbers = $conn->prepare("SELECT * FROM wishlist WHERE name = ? AND user_id = ?");
-      $check_wishlist_numbers->execute([$p_name, $user_id]);
-
-      if($check_wishlist_numbers->rowCount() > 0){
+      if($check_wishlist->rowCount() > 0){
          $delete_wishlist = $conn->prepare("DELETE FROM wishlist WHERE name = ? AND user_id = ?");
          $delete_wishlist->execute([$p_name, $user_id]);
       }
 
       $insert_cart = $conn->prepare("INSERT INTO cart(user_id, pid, name, price, quantity, image) VALUES(?,?,?,?,?,?)");
       $insert_cart->execute([$user_id, $pid, $p_name, $p_price, $p_qty, $p_image]);
-      $message[] = 'added to cart!';
+      $message[] = 'Added to cart!';
    }
-
 }
 
 ?>
@@ -83,97 +71,66 @@ if(isset($_POST['add_to_cart'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>search page</title>
+   <title>Search Page</title>
 
-   <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
-
 </head>
 <body>
    
 <?php include 'header.php'; ?>
 
 <section class="search-form">
-
+   <!-- Ang "value" attribute magpabilin sa imong gi-type maski mo-refresh ang page -->
    <form action="" method="POST">
-      <input type="text" class="box" name="search_box" placeholder="search products...">
-      <input type="submit" name="search_btn" value="search" class="btn">
+      <input type="text" class="box" name="search_box" placeholder="Search products..." value="<?= isset($_POST['search_box']) ? $_POST['search_box'] : ''; ?>" required>
+      <input type="submit" name="search_btn" value="Search" class="btn">
    </form>
-
 </section>
 
-
-<?php
-
-
-
-?>
-
 <section class="products" style="padding-top: 0; min-height:100vh;">
-   <?php
-$search_box = "";
-
-if(isset($_POST['search_box'])){
-   $search_box = $_POST['search_box'];
-  $search_box = htmlspecialchars($_POST['search_box'], ENT_QUOTES, 'UTF-8');
-}
-
-$select_products = $conn->prepare("
-   SELECT * FROM products 
-   WHERE name LIKE ? OR category LIKE ?
-");
-
-$select_products->execute(["%$search_box%", "%$search_box%"]);
-?>
 
    <div class="box-container">
 
    <?php
-if(isset($_POST['search_btn'])){
+      if(isset($_POST['search_btn']) || isset($_POST['search_box'])){
+         $search_box = filter_var($_POST['search_box'], FILTER_SANITIZE_STRING);
+         
+         // Search gamit ang Name o Category
+         $select_products = $conn->prepare("SELECT * FROM products WHERE name LIKE ? OR category LIKE ?");
+         $select_products->execute(["%{$search_box}%", "%{$search_box}%"]);
 
-   $search_box = $_POST['search_box'];
-   $search_box = htmlspecialchars($_POST['search_box'], ENT_QUOTES, 'UTF-8');
-
-   $select_products = $conn->prepare("SELECT * FROM products WHERE name LIKE ? OR category LIKE ?");
-   $select_products->execute(["%$search_box%", "%$search_box%"]);
-
-   if($select_products->rowCount() > 0){
-      while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
-
+         if($select_products->rowCount() > 0){
+            while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
    ?>
    <form action="" class="box" method="POST">
       <div class="price">₱<span><?= $fetch_products['price']; ?></span>/-</div>
       <a href="view_page.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
       <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
       <div class="name"><?= $fetch_products['name']; ?></div>
+      
       <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
       <input type="hidden" name="p_name" value="<?= $fetch_products['name']; ?>">
       <input type="hidden" name="p_price" value="<?= $fetch_products['price']; ?>">
       <input type="hidden" name="p_image" value="<?= $fetch_products['image']; ?>">
+      
       <input type="number" min="1" value="1" name="p_qty" class="qty">
-      <input type="submit" value="add to wishlist" class="option-btn" name="add_to_wishlist">
-      <input type="submit" value="add to cart" class="btn" name="add_to_cart">
+      <input type="submit" value="Add to Wishlist" class="option-btn" name="add_to_wishlist">
+      <input type="submit" value="Add to Cart" class="btn" name="add_to_cart">
    </form>
    <?php
+            }
+         }else{
+            echo '<p class="empty">No result found for "'.htmlspecialchars($search_box).'"!</p>';
          }
       }else{
-         echo '<p class="empty">no result found!</p>';
+         echo '<p class="empty" style="color:var(--light-color);">Please enter a keyword to search.</p>';
       }
-      
-};
    ?>
 
    </div>
 
 </section>
-
-
-
-
-
 
 <?php include 'footer.php'; ?>
 
